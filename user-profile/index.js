0cc351d4205c813ce1ecdd9f6b0d3de6d8fdf380
gcpearse <glen.c.pearse@gmail.com>
2023-11-28 22:24:07 +0000

    retrieves search params from URL for use on the page

---
+++ user-profile/index.js
@@ -0,0 +1,19 @@
+const searchString = document.location.search;
+const queries = new URLSearchParams(searchString);
+const firstName = (queries.get("first-name-input"));
+const surname = (queries.get("surname-input"));
+const username = (queries.get("username-input"));
+const email = (queries.get("email-input"));
+
+const firstNameCell = document.getElementById("first-name-cell");
+const surnameCell = document.getElementById("surname-cell");
+const usernameCell = document.getElementById("username-cell");
+const emailCell = document.getElementById("email-cell");
+
+firstNameCell.innerText = firstName;
+surnameCell.innerText = surname;
+usernameCell.innerText = username;
+emailCell.innerText = email;
+
+const greeting = document.getElementById("greeting");
+greeting.innerText = `Hello, ${firstName} ${surname}!`;
